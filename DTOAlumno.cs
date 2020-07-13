b6c220e043e541a7b28dbb2e0a311598e46b2236
JonasDL <50348980+JonasDL@users.noreply.github.com>
2020-07-12 19:43:51 -0500

    Se quitan atributos en la clase alumno

--- DTOAlumno.cs
+++ DTOAlumno.cs
@@ -23,19 +23,14 @@ namespace FNT_BusinessEntities.Interface
         public string AlumnoCodigoAlumno { get; set; }
         public Int64 AlumnoCodigoPersona { get; set; }
         public string AlumnoCodigoUsuario { get; set; }
-        public string AlumnoEstadoIngreso { get; set; }
         public string AlumnoFlagCorreo { get; set; }
         public string AlumnoCodSede { get; set; }
-        public string PersonaApePatImag { get; set; }
-        public string PersonaApeMatImag { get; set; }
         public string PersonaSexo { get; set; }
-        public string PersonaNombresImag { get; set; }
         public string PersonaApellidoPatern { get; set; }
         public string PersonaApellidoMatern { get; set; }
         public string PersonaNombres { get; set; }
         public string PersonaTipoDocumento { get; set; }
         public string PersonaDocumenIdentida { get; set; }
-        public string LineaNegocioNombre { get; set; }
         public string UsuarioEmail { get; set; }
         public DateTime AlumnoFechaCreacion { get; set; }
         public string AlumnoUsuarioCreacion { get; set; }
